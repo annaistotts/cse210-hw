b244e6ee10435cea76f55eb8ee1cac20ca349e6e
annaistotts <ins17001@byui.edu>
2025-03-29 19:49:08 -0600

    Online Ordering Done

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Text;
+
+public class Order
+{
+    private List<Product> _products = new List<Product>();
+    private Customer _customer;
+
+    public Order(Customer customer)
+    {
+        _customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        _products.Add(product);
+    }
+
+    public double GetTotalCost()
+    {
+        double total = 0;
+        foreach (Product product in _products)
+        {
+            total += product.GetTotalCost();
+        }
+
+        double shipping = _customer.LivesInUSA() ? 5 : 35;
+        return total + shipping;
+    }
+
+    public string GetPackingLabel()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine("Packing Label:");
+        foreach (Product product in _products)
+        {
+            sb.AppendLine(product.GetPackingInfo());
+        }
+        return sb.ToString();
+    }
+
+    public string GetShippingLabel()
+    {
+        return $"Shipping Label:\n{_customer.GetName()}\n{_customer.GetAddress().ToString()}";
+    }
+}
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,33 @@
 using System;
 
+// Again I used ChatGPT to come up with addresses, customer names, and products.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Address and Customer 1 (USA)
+        Address address1 = new Address("123 Center Court", "Orlando", "FL", "USA");
+        Customer customer1 = new Customer("Serena Williams", address1);
+
+        Order order1 = new Order(customer1);
+        order1.AddProduct(new Product("Tennis Racket", "TR123", 120.0, 1));
+        order1.AddProduct(new Product("Wristband Set", "WB456", 15.0, 2));
+
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.GetTotalCost():F2}\n");
+
+        // Address and Customer 2 (International)
+        Address address2 = new Address("89 Grass Lane", "London", "England", "UK");
+        Customer customer2 = new Customer("Andy Murray", address2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(new Product("Grass Court Shoes", "SH789", 95.0, 1));
+        order2.AddProduct(new Product("Headband", "HB321", 12.5, 1));
+        order2.AddProduct(new Product("Grip Tape", "GT654", 6.0, 3));
+
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.GetTotalCost():F2}\n");
     }
 }
